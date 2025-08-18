ef1ac0c9b3500e6bb5210f00318bf601c810b756
Josue Figueroa <josuedaniel03figueroa@gmail.com>
2025-08-17 23:28:08 -0600

    commit arreglado

---
+++ GUIA 1/ejercicio 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Martinez Rosales Josue Daniel carnet: 25-4699-2024
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+            Console.WriteLine("Ingrese un numero: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            n2 = double.Parse(Console.ReadLine());
+            if (n1 > n2)
+            {
+                Console.WriteLine($"El numero mayor es: {n1}");
+            }
+            else if (n2 > n1)
+            {
+                Console.WriteLine($"El mayor es: {n2}");
+            }
+            else
+            {
+                Console.WriteLine("Ambos son iguales");
+            }
+        }
+    }
+}
